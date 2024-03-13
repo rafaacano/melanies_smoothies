e4becfda1d688c54b1b05c49601bb5bbc056890f
rafaacano <74188311+rafaacano@users.noreply.github.com>
2024-03-13 11:31:13 +0100

    Update streamlit_app.py
    response api in json

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
 st.write(
@@ -26,6 +23,9 @@ ingredients_list = st.multiselect(
     ,my_dataframe
     ,max_selections=5
 )
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
 
 if ingredients_list:
     ingredients_string = ''
